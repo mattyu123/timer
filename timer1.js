cb42c01e2d0ff2f62ff5e853df4d34bd73ae1088
mattyu123 <matthewyu100@gmail.com>
2023-05-15 20:35:15 -0400

    cleaned up code with linter

--- timer1.js
+++ timer1.js
@@ -1,15 +1,15 @@
 //Tests edge case if there was no number provided
 if (process.argv.length === 2) {
-  process.stdout.write("a valid argument was not provided \n")
+  process.stdout.write("a valid argument was not provided \n");
 } else {
   for (let i = 2; i < process.argv.length; i++) {
     //if a number is negative or the argument is not a number, that argument will be skipped
-    if (process.argv[i] > 0 || typeof process.argv[i] == Number) {
+    if (process.argv[i] > 0 || typeof process.argv[i] === "number") {
       setTimeout(() => {
-        process.stdout.write('your time is up \n')
-      }, process.argv[i]*1000)
+        process.stdout.write('your time is up \n');
+      }, process.argv[i] * 1000);
     }
-  }  
+  }
 }
 
 
